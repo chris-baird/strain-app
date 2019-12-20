cee721c2d5b8c8c4b9ae97c4a3d2f4f9a11a397f
Chris <chrisbaird05@gmail.com>
2019-12-20 15:35:09 -0800

    Set up folder structure, set up webpack for client side app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import reactImage from '../public/assets/images/react.png';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('hey');
+  }
+  render() {
+    return (
+      <div>
+        <img src={reactImage} alt="" />
+        <button onClick={this.handleClick}>click me</button>;
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+};
